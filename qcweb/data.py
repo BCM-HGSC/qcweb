94b4b1d699e5de2ad6b5a42fae117ae8d07cd34c
jekwatt <jekwatt@users.noreply.github.com>
2018-09-07 13:41:41 -0500

    Switch to using All Time 2007-2018 date

--- qcweb/data.py
+++ qcweb/data.py
@@ -54,6 +54,8 @@ CURRENT_COLUMNS_KEEP = BASE_COLUMNS + ADD1_COLUMNS + ADD2_COLUMNS
 class MyData:
     def __init__(self):
         super(MyData, self).__init__()
+        # self.at = pd.read_pickle('data/2018_at.pickle.gzip')
+        self.at = pd.read_pickle('data/at.pickle.gzip')
         self.at_head = self.at.head()
         print('*** loaded the data with length of', len(self.at))
 
