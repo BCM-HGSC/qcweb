e74ebdb2804aa2f24112eea82af5bc253b448115
Christan Sanchez <cssanchez23@gmail.com>
2018-09-28 10:48:23 -0500

    removed comment and added column name

--- qcweb/data.py
+++ qcweb/data.py
@@ -22,7 +22,7 @@ BASE_COLUMNS = ['LANE_BARCODE',
                 'LIBRARY',
                 'RUN_FINISHED_DATE',
                 'SAMPLE_EXTERNAL_ID',
-                 #'INTERNAL_PROCESSING_SAMPLE_ID',
+                'INTERNAL_PROCESSING_SAMPLE_ID',
                 'MACHINE_NAME',
                 'TOTAL_MB',
                 'REFERENCE_GENOME_ID',
@@ -53,8 +53,7 @@ CURRENT_COLUMNS_KEEP = BASE_COLUMNS + ADD1_COLUMNS + ADD2_COLUMNS
 class MyData:
     def __init__(self):
         super(MyData, self).__init__()
-        # self.at = pd.read_pickle('data/2018_at.pickle.gzip')
-        self.at = pd.read_parquet('data/all_2018-09-26_ID.1932054.parquet')
+        self.at = pd.read_parquet('data/all_2018-09-28_ID.1933796.parquet')
         self.at_head = self.at.head()
         print('*** loaded the data with length of', len(self.at))
 
