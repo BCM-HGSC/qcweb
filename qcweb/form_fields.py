390be8283871ff349556347f193951ffc2331597
jekwatt <jekwatt@users.noreply.github.com>
2018-10-25 13:36:55 -0500

    Update import in form_fields.py

--- qcweb/form_fields.py
+++ qcweb/form_fields.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import (StringField, BooleanField,
-                     DateField, DateTimeField,
+                     DateField, TimeField, DateTimeField,
                      RadioField, SelectField, TextField,
                      TextAreaField, SubmitField, IntegerField)
-from wtforms.validators import DataRequired, InputRequired, Length
+from wtforms.validators import (DataRequired, InputRequired,
+                                Optional, Length)
 
 
 app = Flask(__name__)
