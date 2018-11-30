ec42cf7ff01e4e9b2838a2dc1c201c0de88ddc93
jekwatt <jekwatt@users.noreply.github.com>
2018-11-30 09:37:01 -0600

    Separate ui, unit tests & remove unit test content in test_query_ui.py

--- tests/html/test_query_ui.py
+++ tests/html/test_query_ui.py
@@ -1,42 +1,18 @@
-import datetime
 import time
 
 from pytest import mark
 
-from qcweb import parse_24h_time_str
 
 @mark.skip(reason="demonstration only")
 @mark.ui
-class QueryTests:
-    def test_can_navigate_to_query_page(self, chrome_browser):
-        local_browser = chrome_browser
-        local_browser.get('http://localhost:5000/query')
+def test_can_navigate_to_query_page(chrome_browser):
+    local_browser = chrome_browser
+    local_browser.get('http://localhost:5000/query')
 
-        time.sleep(5)
+    time.sleep(5)
 
-        dev_browser = chrome_browser
-        dev_browser.get('http://submissions-dev.hgsc.bcm.edu/qcweb/main/query')
-        assert True
+    dev_browser = chrome_browser
+    dev_browser.get('http://submissions-dev.hgsc.bcm.edu/qcweb/main/query')
+    assert True
 
-        time.sleep(5)
-
-
-    def test_valid_query(self):
-        assert True
-
-
-    def test_invalid_query(self):
-        assert True
-
-
-    test_time_data = [
-        ('00:00:00', datetime.time(0,0)),
-        ('12:00:00', datetime.time(12, 0)),
-        ('23:59:59', datetime.time(23, 59, 59))
-    ]
-
-
-    @mark.parametrize("time_str, expected", test_time_data)
-    def test_parse_24h_time_str(self, time_str, expected):
-        output = parse_24h_time_str(time_str)
-        assert output == expected
+    time.sleep(5)
