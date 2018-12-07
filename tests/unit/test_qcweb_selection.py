0296e829ebd85f9c51d81bb7bc8ed5813c861c20
jekwatt <jekwatt@users.noreply.github.com>
2018-12-07 13:23:01 -0600

    Add test_datetime_str function in test_qcweb_selection.py

---
+++ tests/unit/test_qcweb_selection.py
@@ -0,0 +1,84 @@
+import datetime
+import pytest
+
+from pytest import mark
+
+
+@mark.unit
+def test_limit_row():
+    assert True
+
+
+ONE_DAY = datetime.timedelta(days=1)
+MIDNIGHT = datetime.time.min
+
+test_datetime_data = [
+    ('2018-05-07', '2018-05-07', '00:00:00', '00:00:00', '2018-05-07T00:00:00', '2018-05-08T00:00:00', 24),
+    ('2018-05-07', '2018-05-08', '00:00:00',  '00:00:00', '2018-05-07T00:00:00', '2018-05-09T00:00:00', 48),
+    ('2018-05-07', '2018-05-07', '00:00:00', '12:00:00', '2018-05-07T00:00:00', '2018-05-07T12:00:00', 12)
+]
+
+@mark.unit
+@mark.parametrize("date_str_start, date_str_end, "
+                  "time_str_start, time_str_end, "
+                  "expected_iso_str_start, expected_iso_str_end, "
+                  "expected_hours", test_datetime_data)
+def test_datetime_str(date_str_start,
+                      date_str_end,
+                      time_str_start,
+                      time_str_end,
+                      expected_iso_str_start,
+                      expected_iso_str_end,
+                      expected_hours):
+    date_start = datetime.datetime.strptime(date_str_start, "%Y-%m-%d").date()
+    time_start = datetime.datetime.strptime(time_str_start, '%H:%M:%S').time()
+    start = datetime.datetime.combine(date_start, time_start)
+
+    date_end = datetime.datetime.strptime(date_str_end, "%Y-%m-%d").date()
+    time_end = datetime.datetime.strptime(time_str_end, '%H:%M:%S').time()
+    end = datetime.datetime.combine(date_end, time_end)
+    if time_end == MIDNIGHT:
+        end += ONE_DAY
+
+    output_iso_str_start = start.isoformat(timespec='seconds')
+    output_iso_str_end = end.isoformat(timespec='seconds')
+    # output_hours = # call the logic being tested here
+
+    assert output_iso_str_start == expected_iso_str_start
+    assert output_iso_str_end == expected_iso_str_end
+    # assert output_hours == expected_hours
+
+
+@mark.unit
+def test_query_ses():
+    assert True
+
+
+@mark.unit
+def test_build_csv_data():
+    assert True
+
+
+@mark.unit
+def test_by_date_range():
+    assert True
+
+
+@mark.unit
+def test_make_range():
+    assert True
+
+
+@mark.unit
+def test_by_plotform():
+    assert True
+
+
+@mark.unit
+def test_by_group():
+    assert True
+
+
+@mark.unit
+def test_by_appl():
+    assert True
