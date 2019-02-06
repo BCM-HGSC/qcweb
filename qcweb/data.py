1da555eafff39f59c7541639afd0aa3adb08557e
jekwatt <jekwatt@users.noreply.github.com>
2019-02-06 14:43:29 -0600

    Load new data all_2019-01-30 parquet

--- qcweb/data.py
+++ qcweb/data.py
@@ -54,7 +54,7 @@ CURRENT_COLUMNS_KEEP = BASE_COLUMNS + ADD1_COLUMNS + ADD2_COLUMNS
 class MyData:
     def __init__(self):
         super(MyData, self).__init__()
-        self.at = pd.read_parquet('data/at.100pct.parquet')
+        self.at = pd.read_parquet('data/all_2019-01-30_ID.2026876.parquet')
         self.at_head = self.at.head()
         self.grp = pd.read_pickle('data/grp.pickle.gzip')
         self.appl = pd.read_pickle('data/appl.pickle.gzip')
